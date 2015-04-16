3912ec6fd9426143ac6d8e5b3dc90fbec5a398b3
subash poudyal <subashpoudyal@lftechnology.com>
2015-04-16 17:45:43 +0545

    minor fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :log_level, :debug
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
